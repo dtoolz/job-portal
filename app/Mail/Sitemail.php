1cd1c07c8ab91decaf0db79cf01b0545402144a5
dtoolz <donaldaudriel@gmail.com>
2023-11-29 18:09:29 +0100

    added mail setup

---
+++ app/Mail/Sitemail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Sitemail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $subject, $body;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($subject, $body)
+    {
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Sitemail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/email/email.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Email from Job Portal</title>
+</head>
+<body>
+    <p>{!! $body !!}</p>
+</body>
+</html>
