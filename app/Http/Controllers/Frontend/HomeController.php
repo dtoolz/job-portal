d050f4c04579e9b9d2e9665c6818baf65f937b0c
dtoolz <donaldaudriel@gmail.com>
2023-12-05 03:20:46 +0100

    showing dynamic content on the frontend home page sections

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\HomePageContent;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('frontend.home');
+        $home_page_content_data = HomePageContent::where('id',1)->first();
+        return view('frontend.home', compact('home_page_content_data'));
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,18 +1,16 @@
 @extends('frontend.layout.app')
 
 @section('main_content')
-    <div class="slider" style="background-image: url({{ asset('uploads/banner5.jpg') }})">
+    <div class="slider" style="background-image: url({{ asset('uploads/'.$home_page_content_data->background) }})">
         <div class="bg"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="item">
                         <div class="text">
-                            <h2>Find Your Desired Job</h2>
+                            <h2>{{$home_page_content_data->heading }}</h2>
                             <p>
-                                Search the best, perfect and suitable jobs
-                                that matches your skills in your expertise
-                                area.
+                                {!! $home_page_content_data->text !!}
                             </p>
                         </div>
                         <div class="search-section">
@@ -22,14 +20,14 @@
                                         <div class="col-lg-3">
                                             <div class="form-group">
                                                 <input type="text" name="" class="form-control"
-                                                    placeholder="Job Title" />
+                                                    placeholder="{{$home_page_content_data->job_title }}" />
                                             </div>
                                         </div>
                                         <div class="col-lg-3">
                                             <div class="form-group">
                                                 <select name="" class="form-select select2">
                                                     <option value="">
-                                                        Job Location
+                                                        {{$home_page_content_data->job_location }}
                                                     </option>
                                                     <option value="">
                                                         Australia
@@ -59,7 +57,7 @@
                                             <div class="form-group">
                                                 <select name="" class="form-select select2">
                                                     <option value="">
-                                                        Job Category
+                                                        {{$home_page_content_data->job_category }}
                                                     </option>
                                                     <option value="">
                                                         Accounting
@@ -79,7 +77,7 @@
                                         <div class="col-lg-3">
                                             <button type="submit" class="btn btn-primary">
                                                 <i class="fas fa-search"></i>
-                                                Search
+                                                {{$home_page_content_data->search }}
                                             </button>
                                         </div>
                                     </div>
